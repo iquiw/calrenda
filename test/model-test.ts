0e38bf90a33f4f8b14dd6f124bac4e6ebbe5015e
Iku Iwasa <iku.iwasa@gmail.com>
2019-08-25 11:15:04 +0900

    Prepare tests for CalRendaModel class

--- test/model-test.ts
+++ test/model-test.ts
@@ -1,7 +1,7 @@
 import test from 'ava';
 
 import { Time } from '../lib/time';
-import { AllDayEntry, PeriodEntry } from '../lib/model';
+import { AllDayEntry, CalRendaModel, PeriodEntry } from '../lib/model';
 
 test('period entry constructor', t => {
   const entry = new PeriodEntry('summary', [[1, 2], [3, 4]]);
@@ -47,4 +47,80 @@ test('allday entry period string', t => {
   const entry = new AllDayEntry('summary');
 
   t.is(entry.periodString, '終日');
+});
+
+test('model constructor', t => {
+  const model = new CalRendaModel();
+
+  t.is(model.rowCount, 0);
+  for (let _entry of model.entries()) {
+    t.fail(); // not executed
+  }
+});
+
+test('model single entry addition', t => {
+  const entry = new AllDayEntry('summary');
+  const model = new CalRendaModel();
+  model.addEntry(entry);
+
+  t.is(model.rowCount, 1);
+  const entries = model.entries();
+  t.deepEqual(entries.next().value, [0, entry]);
+});
+
+test('model multiple entry additions without intersect', t => {
+  const model = new CalRendaModel();
+  const entry1 = new PeriodEntry('summary1', [[1, 0], [2, 0]]);
+  const entry2 = new PeriodEntry('summary2', [[2, 0], [3, 0]]);
+  const entry3 = new PeriodEntry('summary3', [[3, 0], [4, 0]]);
+  model.addEntry(entry1);
+  model.addEntry(entry2);
+  model.addEntry(entry3);
+
+  t.is(model.rowCount, 1);
+  const entries = model.entries();
+  t.deepEqual(entries.next().value, [0, entry1]);
+  t.deepEqual(entries.next().value, [0, entry2]);
+  t.deepEqual(entries.next().value, [0, entry3]);
+  t.true(entries.next().done);
+});
+
+test('model multiple entry additions with intersect', t => {
+  const model = new CalRendaModel();
+  const entry1 = new PeriodEntry('summary1', [[1, 0], [3, 0]]);
+  const entry2 = new PeriodEntry('summary2', [[2, 0], [3, 0]]);
+  const entry3 = new PeriodEntry('summary3', [[2, 0], [4, 0]]);
+  model.addEntry(entry1);
+  model.addEntry(entry2);
+  model.addEntry(entry3);
+
+  t.is(model.rowCount, 3);
+  const entries = model.entries();
+  t.deepEqual(entries.next().value, [0, entry1]);
+  t.deepEqual(entries.next().value, [1, entry2]);
+  t.deepEqual(entries.next().value, [2, entry3]);
+  t.true(entries.next().done);
+});
+
+test('model multiple entry additions with and without intersect', t => {
+  const model = new CalRendaModel();
+  const entry1 = new PeriodEntry('summary1', [[1, 0], [3, 0]]);
+  const entry2 = new PeriodEntry('summary2', [[2, 0], [4, 0]]);
+  const entry3 = new PeriodEntry('summary3', [[3, 0], [5, 0]]);
+  const entry4 = new PeriodEntry('summary4', [[4, 0], [5, 0]]);
+  const entry5 = new PeriodEntry('summary5', [[1, 0], [5, 0]]);
+  model.addEntry(entry1);
+  model.addEntry(entry2);
+  model.addEntry(entry3);
+  model.addEntry(entry4);
+  model.addEntry(entry5);
+
+  t.is(model.rowCount, 3);
+  const entries = model.entries();
+  t.deepEqual(entries.next().value, [0, entry1]);
+  t.deepEqual(entries.next().value, [0, entry3]);
+  t.deepEqual(entries.next().value, [1, entry2]);
+  t.deepEqual(entries.next().value, [1, entry4]);
+  t.deepEqual(entries.next().value, [2, entry5]);
+  t.true(entries.next().done);
 });
